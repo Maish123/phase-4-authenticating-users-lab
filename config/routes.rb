9e424c8a2b427182855fa161e253d8f4c5a9009f
Denis Maina <Denis.maina@student.moringaschool.com>
2022-10-05 17:15:31 +0300

    commit tree 122c58cdedaa9c987faf7210ec9d297f62ee25b7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # for logging in
+  post "/login", to: "sessions#create"
+  # for logging out
+  delete "/logout", to: "sessions#destroy"
+  # for staying logged in
+  get "/me", to: "users#show"
 end
